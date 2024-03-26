2c08651f8345c5e9dc048ffd588b3e649e681252
wenxiwaang <156857298+wenxiwaang@users.noreply.github.com>
2024-03-26 16:47:07 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,59 @@
+let pokemonList = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Pidgeotto","Pidgeot","Rattata","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran","Nidorina","Nidoqueen","Nidoran","Nidorino","Nidoking","Clefairy","Clefable","Vulpix","Ninetales","Jigglypuff","Wigglytuff","Zubat","Golbat","Oddish","Gloom","Vileplume","Paras","Parasect","Venonat","Venomoth","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine","Poliwag","Poliwhirl","Poliwrath","Abra","Kadabra","Alakazam","Machop","Machoke","Machamp","Bellsprout","Weepinbell","Victreebel","Tentacool","Tentacruel","Geodude","Graveler","Golem","Ponyta","Rapidash","Slowpoke","Slowbro","Magnemite","Magneton","Farfetch'd","Doduo","Dodrio","Seel","Dewgong","Grimer","Muk","Shellder","Cloyster","Gastly","Haunter","Gengar","Onix","Drowzee","Hypno","Krabby","Kingler","Voltorb","Electrode","Exeggcute","Exeggutor","Cubone","Marowak","Hitmonlee","Hitmonchan","Lickitung","Koffing","Weezing","Rhyhorn","Rhydon","Chansey","Tangela","Kangaskhan","Horsea","Seadra","Goldeen","Seaking","Staryu","Starmie","Mr. Mime","Scyther","Jynx","Electabuzz","Magmar","Pinsir","Tauros","Magikarp","Gyarados","Lapras","Ditto","Eevee","Vaporeon","Jolteon","Flareon","Porygon","Omanyte","Omastar","Kabuto","Kabutops","Aerodactyl","Snorlax","Articuno","Zapdos","Moltres","Dratini","Dragonair","Dragonite","Mewtwo","Mew"];
+
+// Answers
+
+// Question 1
+
+
+document.getElementById("host-name").innerHTML = 'Wenxi Wang'
+document.getElementById("collaborator-name").innerHTML = 'Yiyang Li'
+document.getElementById("collaborator-2-name").innerHTML = 'None'
+
+// Question 2
+// You can also create their own CSS classes in style.css file and use that. 
+
+
+
+
+// Question 3
+// Try change event first
+// There is another event which fires for every character. This is what we want to use.
+function ExecuteScript()
+{
+document.querySelectorAll('CSS-Selector')[0].click();
+}
+
+
+// Question 4
+// Try change event first
+// There is another event which fires for every character. This is what we want to use.
+
+
+
+// Question 5
+// Try change event first
+// There is another event which fires for every character. This is what we want to use.
+
+
+
+
+//Question 6
+
+
+
+
+
+//Question 7 
+
+
+
+
+
+//Question 8
+
+
+
+
+
+//Question 9 
+
